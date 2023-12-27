16eb5857eb7b45998876d9fafc33400c3473e9af
Alexander Girin <alex.girin.web@gmail.com>
2023-12-27 12:14:53 +0300

    Add linked list

---
+++ src/ds/linked_list.go
@@ -0,0 +1,110 @@
+package ds
+
+import (
+	"cmp"
+	"fmt"
+)
+
+type listNode[T cmp.Ordered] struct {
+	value T
+	next  *listNode[T]
+}
+
+type LinkedList[T cmp.Ordered] struct {
+	head   *listNode[T]
+	length int
+}
+
+func New[T cmp.Ordered]() *LinkedList[T] {
+	return &LinkedList[T]{
+		head:   &listNode[T]{},
+		length: 0,
+	}
+}
+
+func (list *LinkedList[T]) isEmpty() bool {
+	return list.head.next == nil
+}
+
+func (list *LinkedList[T]) Size() int {
+	return list.length
+}
+
+func (list *LinkedList[T]) GetFirst() (T, error) {
+	if list.isEmpty() {
+		return list.head.value, fmt.Errorf("error: %s", "Empty linked list")
+	}
+
+	return list.head.next.value, nil
+}
+
+func (list *LinkedList[T]) GetLast() (T, error) {
+	if list.isEmpty() {
+		return list.head.value, fmt.Errorf("error: %s", "Empty linked list")
+	}
+
+	cur := list.head
+	for cur.next != nil {
+		cur = cur.next
+	}
+
+	return cur.value, nil
+}
+
+func (list *LinkedList[T]) Push(val T) {
+	list.length++
+	node := &listNode[T]{value: val}
+
+	if list.isEmpty() {
+		list.head.next = node
+		return
+	}
+
+	cur := list.head
+	for cur.next != nil {
+		cur = cur.next
+	}
+	cur.next = node
+}
+
+func (list *LinkedList[T]) Pop() (T, error) {
+	if list.isEmpty() {
+		return list.head.value, fmt.Errorf("error: %s", "Empty linked list")
+	}
+
+	cur := list.head
+	for cur.next.next != nil {
+		cur = cur.next
+	}
+	val := cur.next.value
+	cur.next = nil
+	list.length--
+
+	return val, nil
+}
+
+func (list *LinkedList[T]) Enqueue(val T) {
+	node := &listNode[T]{value: val}
+	list.length++
+
+	if list.isEmpty() {
+		list.head.next = node
+		return
+	}
+
+	node.next = list.head.next
+	list.head.next = node
+}
+
+func (list *LinkedList[T]) Dequeue() (T, error) {
+	if list.isEmpty() {
+		return list.head.value, fmt.Errorf("error: %s", "Empty linked list")
+	}
+
+	cur := list.head.next
+	list.head.next = cur.next
+	cur.next = nil
+	list.length--
+
+	return cur.value, nil
+}
---
+++ src/ds/linked_list_test.go
@@ -0,0 +1,103 @@
+package ds
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLinkedListGet(t *testing.T) {
+	var val int
+	var err error
+	list := New[int]()
+
+	val, err = list.GetFirst()
+	assert.NotNil(t, err)
+	assert.Equal(t, 0, val)
+
+	val, err = list.GetLast()
+	assert.NotNil(t, err)
+	assert.Equal(t, 0, val)
+
+	list.Push(10)
+	val, err = list.GetFirst()
+	assert.Nil(t, err)
+	assert.Equal(t, 10, val)
+
+	list.Push(20)
+	list.Push(30)
+	val, err = list.GetLast()
+	assert.Nil(t, err)
+	assert.Equal(t, 30, val)
+}
+
+func TestLinkedListPush(t *testing.T) {
+	list := New[int]()
+
+	list.Push(10)
+	assert.Equal(t, 1, list.Size())
+
+	list.Push(20)
+	assert.Equal(t, 2, list.Size())
+}
+
+func TestLinkedListPop(t *testing.T) {
+	var val int
+	var err error
+	list := New[int]()
+
+	list.Push(10)
+	list.Push(20)
+	assert.Equal(t, 2, list.Size())
+
+	val, err = list.Pop()
+	assert.Nil(t, err)
+	assert.Equal(t, 20, val)
+	assert.Equal(t, 1, list.Size())
+
+	val, err = list.Pop()
+	assert.Nil(t, err)
+	assert.Equal(t, 10, val)
+	assert.Equal(t, 0, list.Size())
+
+	val, err = list.Pop()
+	assert.NotNil(t, err)
+	assert.Equal(t, 0, val)
+}
+
+func TestLinkedListEnqueue(t *testing.T) {
+	list := New[int]()
+
+	list.Enqueue(10)
+	assert.Equal(t, 1, list.Size())
+
+	list.Enqueue(20)
+	assert.Equal(t, 2, list.Size())
+
+	val, err := list.GetFirst()
+	assert.Nil(t, err)
+	assert.Equal(t, 20, val)
+}
+
+func TestLinkedListDequeue(t *testing.T) {
+	var val int
+	var err error
+	list := New[int]()
+
+	list.Push(10)
+	list.Push(20)
+
+	val, err = list.Dequeue()
+	assert.Nil(t, err)
+	assert.Equal(t, 10, val)
+	assert.Equal(t, 1, list.Size())
+
+	val, err = list.Dequeue()
+	assert.Nil(t, err)
+	assert.Equal(t, 20, val)
+	assert.Equal(t, 0, list.Size())
+
+	val, err = list.Dequeue()
+	assert.NotNil(t, err)
+	assert.Equal(t, 0, val)
+}
