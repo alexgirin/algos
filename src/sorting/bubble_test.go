bb170359a93ba2b5afff4f3c1800feed9a55afd2
Alexander Girin <alex.girin.web@gmail.com>
2023-12-28 12:59:17 +0300

    Add test to bubble sort

--- src/sorting/bubble_test.go
+++ src/sorting/bubble_test.go
@@ -28,6 +28,11 @@ func TestBubbleSort(t *testing.T) {
 			input:  []int{13, 1, 8, 2, 5, 3, 1},
 			result: []int{1, 1, 2, 3, 5, 8, 13},
 		},
+		{
+			title:  "Negative/positive elements",
+			input:  []int{13, -1, 8, -2, -5, 3, 1},
+			result: []int{-5, -2, -1, 1, 3, 8, 13},
+		},
 	}
 
 	for _, test := range tests {
